7fec8c927dc60fd7da07efef458b11ed0037cb4f
Jason R. Coombs <jaraco@jaraco.com>
2015-06-13 20:07:03 -0400

    Bump to MongoDB 3

--- jaraco/fabric/mongodb.py
+++ jaraco/fabric/mongodb.py
@@ -1,4 +1,4 @@
-from fabric.api import sudo, task
+from fabric.api import sudo, task, run
 from fabric.contrib import files
 from fabric.context_managers import settings
 
@@ -10,9 +10,23 @@ def distro_install():
     Install mongodb as an apt package (which also configures it as a
     service).
     """
-    content = 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen'
+
+    # MongoDB 2
+    content = (
+        'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart '
+        'dist 10gen',
+    )
     org_list = '/etc/apt/sources.list.d/mongodb.list'
+    #files.append(org_list, content, use_sudo=True)
+
+    # MongoDB 3
+    lsb_release = run("lsb_release -sc")
+    repo_url = "http://repo.mongodb.org/apt/ubuntu"
+    tmpl = "deb {repo_url} {lsb_release}/mongodb-org/3.0 multiverse"
+    content = tmpl.format(**locals())
+    org_list = '/etc/apt/sources.list.d/mongodb-org-3.0.list'
     files.append(org_list, content, use_sudo=True)
+
     with settings(warn_only=True):
         sudo('apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10')
     sudo('aptitude update')
