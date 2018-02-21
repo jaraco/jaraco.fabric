3a9a790798cabcefd1d6be273a37995086707d1e
Jason R. Coombs <jaraco@jaraco.com>
2018-02-21 14:41:35 -0500

    Use Python 3.7 by default

--- jaraco/fabric/python.py
+++ jaraco/fabric/python.py
@@ -4,6 +4,6 @@ from . import apt
 
 
 @task
-def install(version='3.6'):
+def install(version='3.7'):
 	apt.add_ppa('deadsnakes/ppa')
 	apt.install_packages(f'python{version}-dev', f'python{version}-venv')
